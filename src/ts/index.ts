4fc27d45d3db8fba1e4955bd819a29e9df4f6e04
jaen <jaenfigueroa03042000@gmail.com>
2023-04-23 21:45:37 -0500

    add recognition of the pressed key

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -15,14 +15,22 @@ const modal = document.querySelector<HTMLDialogElement>('#modal') as HTMLDialogE
 const modalText = document.querySelector<HTMLParagraphElement>('#modal-text') as HTMLParagraphElement
 const buttonExitModal = document.querySelector<HTMLButtonElement>('#button-exit-modal') as HTMLButtonElement
 
+//PARA CAMBIAR ENTRE LAS SECCIONES DEL JUEGO
 const changeSection = (element1:HTMLElement, element2: HTMLElement) => {
   element1.classList.remove('section--active')
   element2.classList.add('section--active')
 }
 
+//RECONOCER QUE TECLA SE SELECCIONA DURANTE EL JUEGO
+const handleKeyPress = (e:KeyboardEvent) => {
+  const letter = e.key
+  console.log(letter)
+}
+
 //INICIAR JUEGO
 buttonStartGame.addEventListener('click', () => {
   changeSection(section1, section3)
+  window.addEventListener('keypress', handleKeyPress) //EMPEZAR A ESCUCHAR EL TECLADO
 })
 
 //AGREGAR NUEVA PALABRA
@@ -42,19 +50,22 @@ buttonSaveWord?.addEventListener('click', () => {
   }
 })
 
-//DESISTIR DE LA SECCION 2 (AGREGAR NUEVA PALABRA)
+//DESISTIR - SALIR DE AGREGAR NUEVA PALABRA (SECCION 2)
 buttonExitSection2.addEventListener('click', () => {
   changeSection(section2, section1)
 })
 
+//DESISTIR - SALIR DEL JUEGO (SECCION 3)
 buttonExitSection3?.addEventListener('click', () => {
   changeSection(section3, section1)
+  window.removeEventListener('keypress', handleKeyPress) //DEJAR DE ESCUCHAR EL TECLADO
 })
 
-// SALIR DEL MODAL
+//SALIR DEL MODAL
 buttonExitModal?.addEventListener('click', () => {
-  modal.close()
+  modal.close() //OCULTAR MODAL
 })
+
 
 
 
