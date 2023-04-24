7ebf07ebf978cae0e041dccb2ae5dd0cc8482311
jaen <jaenfigueroa03042000@gmail.com>
2023-04-24 13:15:13 -0500

    test: add dependencies for jest

---
+++ src/tests/game.test.ts
@@ -0,0 +1,17 @@
+// 1. Verificar que la función que elige una palabra aleatoria desde una lista de palabras devuelve una palabra de la lista.
+// 2. Asegurarse de que la función que comprueba si una letra está en la palabra oculta devuelve verdadero si la letra está en la palabra y falso si no lo está.
+// 3. Probar que la función que actualiza la palabra oculta con la letra adivinada funciona correctamente.
+// 4. Comprobar que la función que maneja las vidas restantes del jugador se reduce en uno cuando se adivina una letra incorrecta.
+// 5. Verificar que la función que comprueba si el jugador ha perdido devuelve verdadero cuando el número de vidas restantes es cero.
+// 6. Asegurarse de que la función que comprueba si el jugador ha ganado devuelve verdadero cuando la palabra oculta se ha descubierto por completo.
+
+test('probando', () => {
+  expect(2).toEqual(2)
+})
+
+
+
+
+
+
+
