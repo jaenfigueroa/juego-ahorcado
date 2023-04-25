cbacebc5b3b7f95049a6f923fc8664d4fad0b51e
jaen <jaenfigueroa03042000@gmail.com>
2023-04-25 03:29:03 -0500

    refactor: change variable names

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import './ts/pages/'
 import { Game } from './ts/class/game.class.ts'
 
 // recuperar la lista de palabras del local storage
-const stringStorage: string | null = localStorage.getItem('words')
-const storedList: string[] = JSON.parse(stringStorage ?? '["batman"]')
+const storedWordsString: string | null = localStorage.getItem('words')
+const storedWordsList: string[] = JSON.parse(storedWordsString ?? '["batman"]')
 
 //crear mi storage por primera vez
-export const myGame:Game = new Game(storedList)
+export const myGame:Game = new Game(storedWordsList)
 
 
 
