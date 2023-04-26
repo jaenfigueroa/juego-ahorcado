cd04c21c1726ac8d0e87cf99e83bb204bf8140b3
jaen <jaenfigueroa03042000@gmail.com>
2023-04-26 08:58:09 -0500

    doc: add comment to function

--- src/ts/utils/changeColor.utils.ts
+++ src/ts/utils/changeColor.utils.ts
@@ -1,5 +1,6 @@
 import { personSVG } from './domElements.utils';
 
+//CAMBIAR DE COLOR A UNA DE LAS PIEZAS
 export const changeColor = (number: number, color: string) => {
   const rect = personSVG.contentDocument?.getElementById(`piece-${number}`) as HTMLElement
   rect.style.fill = color
