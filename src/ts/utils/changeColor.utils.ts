09ca7037d81db53026584d5d846469562cf03440
jaen <jaenfigueroa03042000@gmail.com>
2023-04-26 08:52:33 -0500

    refactor: improve the changeColor function

--- src/ts/utils/changeColor.utils.ts
+++ src/ts/utils/changeColor.utils.ts
@@ -1,9 +1,7 @@
 import { personSVG } from './domElements.utils';
 
 export const changeColor = (number: number, color: string) => {
-  const rect = personSVG.contentDocument?.getElementById(`piece-${number}`) as HTMLElement | null
-  if (rect) {
-    rect.style.fill = color
-  }
+  const rect = personSVG.contentDocument?.getElementById(`piece-${number}`) as HTMLElement
+  rect.style.fill = color
 }
 
